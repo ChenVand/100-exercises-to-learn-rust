551b74a4ca30c3b53a79181c2321123438839abe
Chenv <chenvd@gmail.com>
2024-09-19 12:55:55 +0300

    lala commit

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,59 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32
+}
+
+impl Order {
+    fn product_name_checker(product_name: &str) {
+        assert!(product_name.len()>0 && product_name.len() <= 300, 
+        "The product name can't be empty and it can't be longer than 300 bytes.");
+    }
+
+    fn quantity_checker(quantity: &u32) {
+        assert!(*quantity>0 ,"The quantity must be strictly greater than zero.");
+    }
+
+    fn unit_price_checker(unit_price: &u32) {
+        assert!(*unit_price>0, "The unit price is in cents and must be strictly greater than zero.");
+    }
+
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Order {
+        Order::product_name_checker(&product_name);
+        Order::quantity_checker(&quantity);
+        Order::unit_price_checker(&unit_price);
+
+        Order{
+            product_name, quantity, unit_price
+        }
+    }
+
+    pub fn product_name(&self) -> &str {&self.product_name}
+
+    pub fn quantity(&self) -> &u32 {&self.quantity}
+
+    pub fn unit_price(&self) -> &u32 {&self.unit_price}
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        Order::product_name_checker(&product_name);
+        self.product_name = product_name;
+    }
+
+    pub fn set_quantity(&mut self, quantity: u32) {
+        Order::quantity_checker(&quantity);
+        self.quantity = quantity;
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: u32) {
+        Order::unit_price_checker(&unit_price);
+        self.unit_price = unit_price;
+    }
+
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+}
